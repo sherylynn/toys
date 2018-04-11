bab25e62b5a20c272c727e44fe7c1612366e0d15
sherylynn <352281674@qq.com>
2018-04-11 13:54:51 +0800

    add emacs learn

---
+++ elisp/learn_X_Y.el
@@ -0,0 +1,12 @@
+(+ 2 2)
+;;C+j insert result
+;;C+x C+e display result
+;;C+x C+s write to file
+(+ 2 (* 3 4))
+;;C+g same to ESC in vim
+;;C+h t open tutorial
+(setq my-name "lynn")
+;;C+/ undo
+;;C+s search forward vim:/ C+r search backward vim:?
+;;C+r in bash
+(insert "hello!")
