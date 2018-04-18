34e8a6295d4b4c1fad059c6bbeff37a5efbd8536
sherylynn <352281674@qq.com>
2018-04-18 23:33:41 +0800

    add batch file to cover init.el for emacs

---
+++ elisp/init_port.bat
@@ -0,0 +1,9 @@
+@echo off
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+@echo off
+cls
+setlocal EnableDelayedExpansion
+@if not exist "%HOME%" @set HOME=%~d0\PortableApps\NodeJSPortable\Data
+call rm "%HOME%\.emacs.d\init.el"
+call mklink "%HOME%\.emacs.d\init.el" "%HOME%\toys\elisp\init.el"
