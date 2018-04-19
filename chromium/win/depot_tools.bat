35bc3d6cfd042703c3e56e4ee600e98cd9375fb5
sherylynn <352281674@qq.com>
2018-04-19 20:28:47 +0800

    踩坑

--- chromium/win/depot_tools.bat
+++ chromium/win/depot_tools.bat
@@ -1,8 +1,11 @@
+::安装依赖的vstudio时，注意要去程序与功能里点开15063的sdk，然后change勾选debug tool
 ::set path for depot_tools first
 ::如果原来有GYP_MSVS_VERSION=2015的环境变量，记得删去
 ::看到官网的set DEPOT_TOOLS_WIN_TOOLCHAIN=0 不知道要不要
 ::万一下载失败，可以gclient sync
 :: go to admin cmd depot_tools path
+:: 使用本地的vstudio
+set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set http_proxy=http://127.0.0.1:10808/
 set https_proxy=http://127.0.0.1:10808/
 gclient
