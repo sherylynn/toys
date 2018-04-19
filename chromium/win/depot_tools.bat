342dfe4e4956ecbbf7358d1d1c710a0bf7af7469
sherylynn <352281674@qq.com>
2018-04-19 15:49:05 +0800

    更新到获取包

--- chromium/win/depot_tools.bat
+++ chromium/win/depot_tools.bat
@@ -4,3 +4,14 @@ set http_proxy=http://127.0.0.1:10808/
 set https_proxy=http://127.0.0.1:10808/
 gclient
 git config --global user.name "sherylynn"
+git config --global user.email "352281674@qq.com"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global branch.autosetuprebase
+git config --global http.proxy "http://127.0.0.1:10808"
+git config --global https.proxy "http://127.0.0.1:10808"
+mkdir chromium && cd chromium
+::slow
+fetch chromium
+::fast
+fetch --no-history chromium 
