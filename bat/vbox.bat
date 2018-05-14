ba95fd13b91ec2190a2dd7bc75e34bd54091c71a
sherylynn <352281674@qq.com>
2018-05-14 14:11:38 +0800

    add vbox cmd

---
+++ bat/vbox.bat
@@ -0,0 +1,10 @@
+@echo off
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+@echo off
+cls
+setlocal EnableDelayedExpansion
+
+set VBOX_CMD=%ProgramFiles%\Oracle\Virtualbox
+cd VBOX_CMD
+pause
