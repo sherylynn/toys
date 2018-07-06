2d17ece08ed8f8e1de332098cf749bc78ff49a15
sherylynn <352281674@qq.com>
2018-07-06 16:55:15 +0800

    update

--- py/hello_world.py
+++ py/hello_world.py
@@ -1,4 +1,9 @@
+greeting='nice to meet u lynn'
 print("hello world")
 print("what is your name?")
 myName=input()
 print("nice to meet you "+ myName)
+if myName=='lynn':
+    print(greeting)
+else
+    print('greeting')
