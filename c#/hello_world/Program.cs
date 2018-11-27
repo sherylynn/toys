c57863ea560c72200d35b3ab4019f7402153d18b
sherylynn <352281674@qq.com>
2018-11-27 21:58:44 +0800

    update msdn tutorial

--- c#/hello_world/Program.cs
+++ c#/hello_world/Program.cs
@@ -6,7 +6,34 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new BankAccount("lynn",1000);
+            Console.WriteLine($"account {account.Number} Owner is {account.Owner} ");
+            var account2 = new BankAccount("lynn", 1000);
+            Console.WriteLine($"account {account2.Number} Owner is {account2.Owner} ");
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(1000, DateTime.Now, "friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch(ArgumentNullException e)
+            {
+                Console.WriteLine("Exception caught creating account winth negative balance");
+                Console.WriteLine(e.ToString());
+            }
+            try
+            {
+                account.MakeWithdrawal(10000, DateTime.Now, "attempt to overdraw");
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine("exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine(account.GetAccountHistory());
         }
     }
 }
---
+++ c#/hello_world/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace hello_world
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount,DateTime date,string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
