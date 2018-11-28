efc8d18fefc49661290f992c3e534073b0239056
sherylynn <352281674@qq.com>
2018-11-28 21:43:52 +0800

    add new learn to c#

---
+++ c#/player/EmptyInterface.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace TeleprompterConsole
+{
+    internal class TelePrompterConfig
+    {
+        private object lockHandle = new object();
+        public int DelayInMilliseconds { get; private set; } = 200;
+    }
+}
