4a068ef7e61b35863f73ffc25ea361a6bd835024
sherylynn <352281674@qq.com>
2018-12-29 10:57:46 +0800

    add new test

--- ts/deno.ts
+++ ts/deno.ts
@@ -13,5 +13,40 @@ function greeter(person:Person){
   return "Hello, " + person.firstName+person.lastName;
 }
 let user:Person=new Student("shery","L.","lynn");
+console.log(user.firstName)
 
 console.log(greeter(user));
+class Animal {
+  move(distanceInMeters:number=0):void{
+    console.log(`Animal moved $(distanceInMeters)m.`);
+  }
+}
+class Cat extends Animal{
+  bark():void{
+    console.log('Moew! Moew!');
+  }
+}
+const ugly:Cat=new Cat();
+ugly.bark()
+
+class Greeter{
+  static standardGreeting="Hello, there";
+  greeting:string;
+  greet():string{
+    if(this.greeting){
+      return "Hellow, "+ this.greeting;
+    }else{
+      return Greeter.standardGreeting;
+    }
+  };
+  show():void{
+    console.log(this.greet());
+  }
+}
+let greeter1:Greeter;
+greeter1=new Greeter();
+greeter1.show();
+let greeterMaker:typeof Greeter =Greeter;
+Greeter.standardGreeting="hello"
+console.log(Greeter.standardGreeting);
+let myAdd=(x,y)=>{return x+y }
