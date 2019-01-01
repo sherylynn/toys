28dfe908859f60b799d9bd3a825fd575147f1b35
sherylynn <352281674@qq.com>
2019-01-01 15:53:08 +0800

    python lesson1

---
+++ py/lesson1/hello_lover.py
@@ -0,0 +1,10 @@
+print('i love lz')
+#要用cd来切换目录，当前目录是cd ./  上一级目录是 cd ..
+#查看所有的目录内内容 dir 
+# cd = change directory 
+# dir = directory
+
+# 通过 运行python 交互式环境中  >>> 特点 :直接获得运行结果，但是不能保存
+# 用编辑器 vscode 运行某一个py文件，需要在命令行 python 文件名.py
+#                           python ./文件名.py
+# 编辑器保存 ctrl+s[ave] windows 在mac command + s[ave]
