e1176a94f667d5ab10cf8b1c565ef9ca1ae707b6
sherylynn <352281674@qq.com>
2019-01-02 19:41:53 +0800

    lesson2

--- py/lesson1/hello_lover.py
+++ py/lesson1/hello_lover.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 print('i love lz')
 #要用cd来切换目录，当前目录是cd ./  上一级目录是 cd ..
 #查看所有的目录内内容 dir 
@@ -8,3 +9,14 @@ print('i love lz')
 # 用编辑器 vscode 运行某一个py文件，需要在命令行 python 文件名.py
 #                           python ./文件名.py
 # 编辑器保存 ctrl+s[ave] windows 在mac command + s[ave]
+print("i love lz\'s cat:\n\"meow\"")
+
+print(r'''love u zob zob
+U love me too''')
+name='lanlan'
+name='lz'
+print(name)
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
