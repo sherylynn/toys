3b0f9fefd97caa127f74a919ef6e353dc776cfe9
sherylynn <352281674@qq.com>
2019-01-14 14:03:33 +0800

    add copy for testhelper

---
+++ c#/copy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace copy
+{
+  class Program
+  {
+    static string FileName = @"winLanguage.dll";
+    static string SourceDir = Path.GetFullPath(FileName);
+    static string TargetDir = @"C:\Files";
+    //static void Main(string[] args = ( FileName,SourceDir,TargetDir))
+    static void Main(string[] args)
+    {
+      try
+      {
+        File.Copy(SourceDir,Path.Combine(TargetDir,FileName),true);
+        Console.WriteLine("已经安装");
+      }
+      catch (IOException copyError)
+      {
+        Console.WriteLine(copyError.Message);
+      }
+      //Console.WriteLine("now directory name is {0}", SourceDir);
+    }
+  }
+}
