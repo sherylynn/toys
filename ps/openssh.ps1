fa7a947249e674e4332195dcac74d05695b43f63
sherylynn <352281674@qq.com>
2019-01-14 11:20:59 +0800

    finish c# testhelp

---
+++ ps/openssh.ps1
@@ -0,0 +1,6 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$url = 'https://github.com/PowerShell/Win32-OpenSSH/releases/latest/'
+$request = [System.Net.WebRequest]::Create($url)
+$request.AllowAutoRedirect=$false
+$response=$request.GetResponse()
+$([String]$response.GetResponseHeader("Location")).Replace('tag','download') + '/OpenSSH-Win64.zip'
