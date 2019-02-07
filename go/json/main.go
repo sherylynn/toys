3cf989dd1ec62d8c3ac0f1c657e11e7396cb5b4c
sherylynn <352281674@qq.com>
2019-02-08 00:15:17 +0800

    add json reader

--- go/json/main.go
+++ go/json/main.go
@@ -3,14 +3,37 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-
-	"github.com/sherylynn/fuego"
 )
 
-func install() {
-	fmt.Print("help")
+type Pack struct {
+	Name      string
+	Version   string
+	Url       string
+	Extension string
+	Arch      struct {
+		Arm   string
+		Amd64 string
+		I386  string
+	}
+	Os struct {
+		Linux   string
+		Windows string
+		Darwin  string
+	}
 }
 
 func main() {
-	fuego.Fire(install)
+	packJson := `{
+  "name":"v2ray",
+  "version":"4.11.0",
+  "url":"https://github.com/v2ray/v2ray-core/releases/download/v$version/v2ray-$os-$arch.zip",
+  "extension":"zip",
+  "arch":{"armv7l":"arm","armv8l":"arm","AMD64":"64","x86_64":"64"},
+  "os":{"Linux":"linux","Windows":"windows","Darwin":"macos"},
+  "comment":"https://github.com/v2ray/v2ray-core/releases/download/v4.11.0/v2ray-linux-arm.zip"
+	}`
+	var pack Pack
+	json.Unmarshal([]byte(packJson), &pack)
+	fmt.Println(pack)
+	fmt.Println(pack.Os.Darwin)
 }
