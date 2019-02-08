54d693d9f13c4caca4adf2ada535ecd245564553
sherylynn <352281674@qq.com>
2019-02-08 16:22:10 +0800

    add map to struct

--- go/json/main.go
+++ go/json/main.go
@@ -10,15 +10,17 @@ type Pack struct {
 	Version   string
 	Url       string
 	Extension string
-	Arch      struct {
-		Arm    string
-		Armv7l string
-		Armv8l string
-		Amd64  string
-		I386   string
-		X86_64 string
-	}
-	Os struct {
+	/*
+		Arch      struct {
+			Arm    string
+			Armv7l string
+			Armv8l string
+			Amd64  string
+			I386   string
+			X86_64 string
+		}*/
+	Arch map[string]string
+	Os   struct {
 		Linux   string
 		Windows string
 		Darwin  string
@@ -38,5 +40,6 @@ func main() {
 	var pack Pack
 	json.Unmarshal([]byte(packJson), &pack)
 	fmt.Println(pack)
+	fmt.Println(pack.Arch["x86_64"])
 	fmt.Println(pack.Os.Darwin)
 }
