3ec16afbc2e74fd1c60e1f2e813ddbbe5c70a17b
sherylynn <352281674@qq.com>
2019-02-16 07:40:33 -0500

    add http server for golang

--- go/helloworld/main.go
+++ go/helloworld/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
-	"github.com/sherylynn/toys/go/zip"
+	"log"
+	"net/http"
+	//"github.com/sherylynn/toys/go/zip"
 )
 
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world")
+}
 func main() {
 	fmt.Println("hello 世界")
-	zip.Unzip("test.zip", "./")
+	//zip.Unzip("test.zip", "./")
 	//zip.Unzip("~/toys/go/zip/test.zip", "~/toys/go/zip")
+	http.HandleFunc("/", HelloWorld)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
