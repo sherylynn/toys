1dc33d187f259c79a1e73e0b7bbaf0486bdb9c2e
sherylynn <352281674@qq.com>
2019-08-02 17:20:47 +0800

    add more command for apktool process

--- java/installer.sh
+++ java/installer.sh
@@ -1,2 +1,9 @@
 installer(){ local apk=$1; local byte=$(wc -c < ${apk}); cat $apk | pm install -S $byte ;}
 # installer apk_path
+
+## apk 反编译
+apktool d aide.apk
+## apk 重新编译
+apktool.bat b aide -o aide_new.apk
+## jarsigner 重新签名
+jarsigner -verbose -keystore testUtils.jks -signedjar aide_sign.apk aide_new.apk testUtils
