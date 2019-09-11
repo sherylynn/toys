47f7352a25e1cec186945cbb5db4f48db33490ee
sherylynn <352281674@qq.com>
2019-09-11 23:10:39 +0800

    fix jupyter output to stderr

--- py/subprocess_test.py
+++ py/subprocess_test.py
@@ -1,16 +1,18 @@
 import subprocess
 p = subprocess.Popen('ping www.baidu.com',shell=True,stdout=subprocess.PIPE)
 while 1:
-    line=p.stdout.readline()
-    if "time" in line:
+    line=p.stdout.readline().decode('utf-8')
+    if 'time' in line:
         print(line)
         break
 
 #是否是shell不影响jupyter依然会把输出跑到终端
+#发现用的是stderr
 #q = subprocess.Popen('jupyter notebook --port=8887',shell=True,stdout=subprocess.PIPE)
-q = subprocess.Popen(['jupyter','notebook','--port=8887'],stdout=subprocess.PIPE)
+q = subprocess.Popen(['jupyter','notebook','--port=8887'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 while 1:
-    line2=q.stdout.readline()
+    line2=q.stderr.readline().decode('utf-8')
     if "http" in line2:
         print(line2)
+        print(line2.split("http")[1])
         break
