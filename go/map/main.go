53541e8bbcb6fce78215a5be9a32b5dc3b279ae8
sherylynn <352281674@qq.com>
2019-09-25 17:13:21 +0800

    update

--- go/map/main.go
+++ go/map/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-
+	//"strconv"
 	jsoniter "github.com/json-iterator/go"
 )
 
@@ -12,7 +12,9 @@ func main() {
 	fmt.Println(pageNum)
 	json := jsoniter.ConfigCompatibleWithStandardLibrary
 
-	bookMap := make(map[string]interface{})
+	//bookMap := make(map[string]interface{})
+	//bookMap := make(map[string]string)
+	bookMap := make(map[string]int)
 	json.Unmarshal([]byte(str), &bookMap)
 	fmt.Println(bookMap)
 	bookMap["fuck"] = 2
@@ -22,4 +24,19 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Printf("%s", jsonStr)
+	fmt.Println(bookMap["test"])
+	fmt.Println(bookMap["noValue"])
+	/*
+		if bookMap["noValue"] == nil {
+			fmt.Println("empty and nil")
+		}
+		if bookMap["noValue"] == "" {
+			fmt.Println("empty and ''")
+		}
+	*/
+	if bookMap["noValue"] == 0 {
+		fmt.Println("empty and 0")
+	}
+	//fmt.Println(nil.ToString())
+	//fmt.Println(strconv.ParseInt(bookMap["test"].ToString()))
 }
