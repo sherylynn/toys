171681f55ebb0dded31ed299e7165fe017f2ea73
sherylynn <352281674@qq.com>
2019-12-17 01:08:51 +0800

    add depends for building vlc-android on mac

---
+++ android/mac_vlc_build_depend.sh
@@ -0,0 +1,6 @@
+brew cask install adoptopenjdk
+brew install ant
+brew install autoconf libtool automake make m4 gawk gcc pkg-config cmake protobuf ragel
+brew install flex subversion unzip git python gettext coreutils
+brew install ninja bison nasm meson
+
