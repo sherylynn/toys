ad109bcaa37cb91c85d580e39baa8e105cc32b97
sherylynn <352281674@qq.com>
2020-02-19 17:31:49 +0800

    process.cwd is different with __dirname

--- js/golang.js
+++ js/golang.js
@@ -1,1 +1,11 @@
+var fs=require("fs")
 console.log("the pwd is:"+__dirname)
+workdir={
+  "__dirname":__dirname,
+  "process.cwd()":process.cwd()
+}
+workdir_new=JSON.stringify(workdir,null,"\t")
+fs.writeFile('golang_nodejs.json',workdir_new, 'utf8', (err) => {
+  if (err) throw err;
+  console.log('done');
+});
