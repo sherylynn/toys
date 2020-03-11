b23cd8e59595d3f1dca255651e5fd7d4f039735f
sherylynn <352281674@qq.com>
2020-03-11 12:28:48 +0800

    add more test for parameter

--- sh/parameter.sh
+++ sh/parameter.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 echo $0
 echo $1
+echo $2
+
+if [[ $1 != "" ]]; then
+  echo "\$1 is not empty"
+else
+  echo "\$1 is empty"
+fi
+
+args2=$2
+
+if [[ $args2 != "" ]]; then
+  echo "args2 is not empty"
+else
+  echo "args2 is empty"
+fi
