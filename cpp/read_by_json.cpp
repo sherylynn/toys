6d8dd46715aeb5f59bdc24dfade6658ada40a31e
sherylynn <352281674@qq.com>
2021-02-27 00:52:13 +0800

    add more for read json of cpp

--- cpp/read_by_json.cpp
+++ cpp/read_by_json.cpp
@@ -2,11 +2,21 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <string>
+int get_resolution_width(){
+  
+}
 int main() {
   using json = nlohmann::json;
   std::ifstream input("resolution.json");
   json j;
   input >> j;
-  std::cout << j["default"] << std::endl;
+  std::string default_resolution;
+  default_resolution = j["default"];
+  int high;
+  int width;
+  high=j[default_resolution]["h"];
+  width=j[default_resolution]["w"];
+  std::cout << high << std::endl;
   return 0;
 }
