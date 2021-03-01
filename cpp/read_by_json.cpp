a1143f236a10fe0e5d0f54237453b0d843050c5e
sherylynn <352281674@qq.com>
2021-03-01 10:43:40 +0800

    split function by json cpp

--- cpp/read_by_json.cpp
+++ cpp/read_by_json.cpp
@@ -3,35 +3,43 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-int get_resolution_width(std::string path){
+
+nlohmann::json get_resolution_json(std::string path){
   using json = nlohmann::json;
   std::ifstream input(path);
   json j;
   input >> j;
-  std::string default_resolution;
-  default_resolution = j["default"];
-  int high;
+  return j;
+}
+
+nlohmann::json get_default_resolution(nlohmann::json resolution_json,std::string default_label="default"){
+  //get default resolution size by read default_label
+  std::string default_resolution_size;
+  default_resolution_size=resolution_json[default_label];
+  //get detail default resolution json by default resolution size
+  nlohmann::json default_resolution_json;
+  default_resolution_json=resolution_json[default_resolution_size];
+  return default_resolution_json;
+}
+int get_resolution_width(nlohmann::json default_resolution_json){
   int width;
-  width=j[default_resolution]["w"];
+  width=default_resolution_json["w"];
   return width;
 }
-int get_resolution_high(std::string path){
-  using json = nlohmann::json;
-  std::ifstream input(path);
-  json j;
-  input >> j;
-  std::string default_resolution;
-  default_resolution = j["default"];
+int get_resolution_high(nlohmann::json default_resolution_json){
   int high;
-  int width;
-  high=j[default_resolution]["h"];
+  high=default_resolution_json["h"];
   return high;
 }
 int main() {
   std::string path;
   path="resolution.json";
+  nlohmann::json resolution_json;
+  resolution_json=get_resolution_json(path);
+  nlohmann::json default_resolution_json;
+  default_resolution_json=get_default_resolution(resolution_json);
   int high;
-  high=get_resolution_high(path);
+  high=get_resolution_high(default_resolution_json);
   std::cout << high << std::endl;
   return 0;
 }
