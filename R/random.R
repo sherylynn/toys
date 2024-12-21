2d0406476ac50f442f0129fa3dec8c57325d0d1d
sherylynn <352281674@qq.com>
2024-12-21 11:19:05 +0800

    add fmt for R

--- R/random.R
+++ R/random.R
@@ -1,4 +1,11 @@
+# 清华源 bioconductor
+options(BioC_mirror = "https://mirrors.tuna.tsinghua.edu.cn/bioconductor")
+# styler 格式化代码
+if (!require("styler", quietly = TRUE)) {
+  install.packages("styler")
+}
 a=1:6
 ls()
 mean(a)
 sample(x=1:4,size=2)
+sample(x=a,size=1)
