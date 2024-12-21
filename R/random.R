d124b4f9220d6c3a6b241b56465f0a729b90a542
sherylynn <352281674@qq.com>
2024-12-21 15:36:28 +0800

    add gplot

--- R/random.R
+++ R/random.R
@@ -1,9 +1,16 @@
 # 清华源 bioconductor
 options(BioC_mirror = "https://mirrors.tuna.tsinghua.edu.cn/bioconductor")
-# styler 格式化代码
-if (!require("styler", quietly = TRUE)) {
-  install.packages("styler")
+my_install <- function(my_package) {
+  if (!require(my_package, quietly = TRUE)) {
+    install.packages(my_package)
+  }
 }
+
+
+# styler 格式化代码
+my_install("styler")
+# 可视化
+my_install("ggplot2")
 # 获取所有对象空间
 # ls()
 # 获取平均值
@@ -19,3 +26,7 @@ roll <- function(bones = 1:6) {
 # roll()
 roll(1:6)
 # roll(bones = 1:20)
+library("ggplot2")
+x <- c(1, 2, 3, 4, 5)
+y <- x * 2
+qplot(x, y)
