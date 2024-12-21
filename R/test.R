614eb38cb5d72aa382d04f59c8212814260349b4
sherylynn <352281674@qq.com>
2024-12-21 16:45:43 +0800

    add test.R

---
+++ R/test.R
@@ -0,0 +1,14 @@
+# 清华源 bioconductor
+options(BioC_mirror = "https://mirrors.tuna.tsinghua.edu.cn/bioconductor")
+# 安装shiny包
+#if (!requireNamespace("shiny", quietly = TRUE)) {
+#  install.packages("shiny")
+#}
+#!requireNamespace("shiny", quietly = TRUE)
+#!require("shiny", quietly = TRUE)
+#!require("shiny")
+#.libPaths()
+#file.exists(file.path(.libPaths(), "ggplot2"))
+#library('ggplot2')
+#library('shiny')
+require('styler')
