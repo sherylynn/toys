cad1a211d90b0b3d33c4d073962be61de5b1b9e5
sherylynn <352281674@qq.com>
2024-12-25 17:45:38 +0800

    [2024-12-25 三 17:45]

--- R/random.R
+++ R/random.R
@@ -2,22 +2,26 @@
 # ls()
 # 获取平均值
 # mean(a)
-setwd('~/toys/R')
+setwd("~/toys/R")
 library("httpgd")
 dev.off()
 hgd()
 roll <- function(bones = 1:6) {
   # a <- 1:6
   # dice <- sample(x = a, size = 2, replace = TRUE)
-  dice <- sample(x = bones, size = 2, replace = TRUE,
-                 prob = c(1/8,1/8,1/8,1/8,1/8,3/8)
-                 )
+  dice <- sample(
+    x = bones, size = 2, replace = TRUE,
+    prob = c(1 / 8, 1 / 8, 1 / 8, 1 / 8, 1 / 8, 3 / 8)
+  )
   sum(dice)
 }
 # 获取函数参数
 # args(sample)
 # roll()
+die <- 1:6
+names(die) <- c("one", "two", "three", "four", "fivr", "six")
 roll(1:6)
+names(die)
 # roll(bones = 1:20)
 library("ggplot2")
 x <- c(1, 2, 3, 4, 5)
@@ -29,14 +33,13 @@ x2 <- c(1, 2, 2, 2, 3, 4, 4, 4, 5)
 p2 <- qplot(x2, binwidth = 1)
 ggsave("/sdcard/Download/qplot2.pdf", plot = p2, width = 10, height = 10)
 
-rolls=replicate(10000,roll())
-p3=qplot(rolls,binwidth=1)
+rolls <- replicate(10000, roll())
+p3 <- qplot(rolls, binwidth = 1)
 ggsave("/sdcard/Download/qplot3.pdf", plot = p3, width = 10, height = 10)
 
-#不带变量存储就直接绘图
-qplot(rolls,binwidth=1)
-#library("txtplot")
-#?txtplot
-#!require("txtplot")
-#system('pwd')
-
+# 不带变量存储就直接绘图
+qplot(rolls, binwidth = 1)
+# library("txtplot")
+# ?txtplot
+# !require("txtplot")
+# system('pwd')
