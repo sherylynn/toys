260338541f9f666cec0a5416eb63a012bf689acb
sherylynn <352281674@qq.com>
2025-01-12 16:01:13 +0800

    update

--- R/test.R
+++ R/test.R
@@ -1,14 +1,1 @@
-#.libPaths()
-#file.exists(file.path(.libPaths(), "ggplot2"))
-#library('ggplot2')
-#library('shiny')
-source("my_package.R")
-install_all()
-require('styler')
-#?sample
-test=c(1,2,3)
-#names(test)=c('q')
-# 相关性包
-#corrplot
-#printer(width=79) #这个是原始 S 语言中的功能，建议用 txtplot 作为 txt 界面下的替代选择
-
+setwd("~/toys/R")
