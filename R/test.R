6255afecfcf059472c0d1689d3e594e8a9e3ddcf
sherylynn <352281674@qq.com>
2025-01-13 17:41:12 +0800

    add new test

--- R/test.R
+++ R/test.R
@@ -1,1 +1,8 @@
-setwd("~/toys/R")
+# setwd("~/toys/R")
+
+library(dplyr)
+A <- read.csv("file1.csv")
+B <- read.csv("file2.csv")
+result <- anti_join(A, B, "身份证号码")
+# help(write.csv)
+write.csv(result, file = "result.csv", fileEncoding = "UTF-8")
