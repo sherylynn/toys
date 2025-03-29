68664ff25eb44f580003b66d9745c16c1deab2e5
sherylynn <352281674@qq.com>
2025-03-29 11:31:06 +0800

    small fix

--- AI-stock/stockviewer/app/build.gradle.kts
+++ AI-stock/stockviewer/app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.preference:preference-ktx:1.2.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
